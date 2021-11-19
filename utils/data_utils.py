bdf59d11b97f3343785b1b13c547f21675a4c5d6
Witold Wydmański <wwydmanski@gmail.com>
2021-11-19 09:32:22 +0100

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -16,12 +16,12 @@ def get_loader(args):
     transform_train = transforms.Compose([
         transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        transforms.Normalize(mean=[0.5], std=[0.5]),
     ])
     transform_test = transforms.Compose([
         transforms.Resize((args.img_size, args.img_size)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        transforms.Normalize(mean=[0.5], std=[0.5]),
     ])
 
     if args.dataset == "cifar10":
@@ -33,7 +33,15 @@ def get_loader(args):
                                    train=False,
                                    download=True,
                                    transform=transform_test) if args.local_rank in [-1, 0] else None
-
+    elif args.dataset == "fashion-mnist":
+        trainset = datasets.FashionMNIST(root="./data",
+                                    train=True,
+                                    download=True,
+                                    transform=transform_train)
+        testset = datasets.FashionMNIST(root="./data",
+                                   train=False,
+                                   download=True,
+                                   transform=transform_test) if args.local_rank in [-1, 0] else None
     else:
         trainset = datasets.CIFAR100(root="./data",
                                      train=True,
